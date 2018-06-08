b09f80e189eaba83bbee00545e9eed43049ff21f
Alexander Nesterenko <alexcleac@protonmail.com>
2018-06-08 11:41:04 +0300

    [sh] Improve usability of roj.sh

--- bin/roj.sh
+++ bin/roj.sh
@@ -1,27 +1,29 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 project=$1
+ROJDIR="$HOME/workspace/$project"
 
 if [ -z "$project" ]; then
    echo 'No project specified: please type project name'
-   exit 1
+   exit 2
 fi
 
-
-ROJDIR="$HOME/workspace/$project"
-SUBSCRIPT_NAME=.rojsh
+function create-prompt() {
+    echo -n "Project $project does not exist. Would you like to create it? [yN] "
+    read v
+    if [ 'y' != $v ]; then
+        exit 1
+    else
+        mkdir -p "$ROJDIR"
+    fi
+}
 
 tmux attach -t "$project" 2> /dev/null
 
 if [ ! $? -eq 0 ]; then
    if [ ! -d "$ROJDIR" ]; then
-      mkdir -p "$ROJDIR"
+       create-prompt;
    fi
-   if ([ -n "$ROJDIR/$SUBSCRIPT_NAME" ] && [ ! -z "$(cat "$ROJDIR/$SUBSCRIPT_NAME" 2> /dev/null)" ]); then
-      start_script="$SHELL \"$ROJDIR/$SUBSCRIPT_NAME; $SHELL\""
-   else
-      start_script=$SHELL
-   fi
-   tmux new -s "$project" -c "$ROJDIR" $start_script
+   tmux new -s "$project" -c "$ROJDIR"
 fi
 
