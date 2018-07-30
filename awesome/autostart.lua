c764b6617d9047566bb720b502f43048f2f1762b
Alexander Nesterenko <alexcleac@protonmail.com>
2018-07-30 18:10:26 +0300

    [awesome] switch to bash shell for autostart

--- awesome/autostart.lua
+++ awesome/autostart.lua
@@ -1,7 +1,8 @@
+local os = os
 local awful = require('awful')
-local sh_interpreter = 'sh '
-local autostart_file_source = '~/.config/awesome/autostart '
-local home_root_file_source = '~/.autostart'
+local sh_interpreter = '/bin/bash '
+local autostart_file_source = os.getenv('HOME') .. '/.config/awesome/autostart '
+local home_root_file_source = os.getenv('HOME') .. '/.autostart'
 
 local function invoke()
   awful.util.spawn_with_shell(sh_interpreter .. autostart_file_source)
